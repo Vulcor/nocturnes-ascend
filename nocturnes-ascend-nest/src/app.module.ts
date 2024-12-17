bf9e24cd227b1074a0c49648cc12e84437587a75
Vulcor <Damnrottlet@gmail.com>
2024-12-17 02:17:14 +0200

    feature/init-server
    - add validation to env variables

--- nocturnes-ascend-nest/src/app.module.ts
+++ nocturnes-ascend-nest/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { validationSchema } from './validation.schema';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      validationSchema: validationSchema,
       isGlobal: true,
     }),
   ],
