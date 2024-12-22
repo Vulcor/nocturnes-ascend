3add967ff3ad9d147a9c5d8fd86e3d56b8db4881
Vulcor <Damnrottlet@gmail.com>
2024-12-22 19:13:06 +0200

    feature/init-database
    - fix default value for NODE_ENV variable

--- nocturnes-ascend-nest/src/validation.schema.ts
+++ nocturnes-ascend-nest/src/validation.schema.ts
@@ -3,7 +3,7 @@ import * as Joi from 'joi';
 export const validationSchema = Joi.object({
   NODE_ENV: Joi.string()
     .valid('development.local', 'production.local')
-    .default('develop'),
+    .default('development.local'),
 
   NEST_PORT: Joi.number().default(3001),
   MONGO_URL: Joi.string().required(),
