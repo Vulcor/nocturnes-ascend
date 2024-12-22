5152f483a3aa5a50601bcc8cb819ab7715b7d000
Vulcor <Damnrottlet@gmail.com>
2024-12-22 19:09:55 +0200

    feature/init-database
    - set up a docker-compose for a conteinerized DB;
    - move swagger config to a separate file in core config;
    - add dockerfile example for future work;
    - update env variables

--- nocturnes-ascend-nest/src/main.ts
+++ nocturnes-ascend-nest/src/main.ts
@@ -1,6 +1,6 @@
-import { initSwagger } from './core/swagger/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { initSwagger } from './core/config/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
--- nocturnes-ascend-nest/src/validation.schema.ts
+++ nocturnes-ascend-nest/src/validation.schema.ts
@@ -1,7 +1,12 @@
 import * as Joi from 'joi';
 
 export const validationSchema = Joi.object({
-  NODE_ENV: Joi.string().valid('develop', 'production').default('develop'),
+  NODE_ENV: Joi.string()
+    .valid('development.local', 'production.local')
+    .default('develop'),
 
   NEST_PORT: Joi.number().default(3001),
+  MONGO_URL: Joi.string().required(),
+  MONGO_USERNAME: Joi.string().required(),
+  MONGO_PASSWORD: Joi.string().required(),
 });
